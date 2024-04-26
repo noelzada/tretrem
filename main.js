9469fbb18717eb56a04b335831f94ad4f61fe25e
nicolads <111922549+noelzada@users.noreply.github.com>
2024-04-26 10:25:25 -0300

    sada

--- main.js
+++ main.js
@@ -15,9 +15,9 @@ for (let i = 0; i < botoes.length; i++) {
 }
 
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2024-07-03T07:00:00");
-const tempoObjetivo2 = new Date("2025-02-01T07:00:00");
-const tempoObjetivo3 = new Date("2024-09-06T07:00:00");
+const tempoObjetivo1 = new Date("2024-07-03T01:09:00");
+const tempoObjetivo2 = new Date("2025-02-01T09:04:00");
+const tempoObjetivo3 = new Date("2024-09-06T03:02:00");
 const tempoObjetivo4 = new Date("2024-06-28T07:00:00");
 
 const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
